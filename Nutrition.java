448866ff45b01d6d8c9d3016c714ace686a28b50
Ashley Wonders <ADW79@pitt.edu>
2021-05-23 23:20:15 -0400

    Initial Commit Nutrition Program Plan

---
+++ Nutrition.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/**
+* I want this file to calculate and track the following:
+* Calories, Protein, Carbohydrates, Fiber from meals
+* Date and Time of meals
+* Blood Sugar with an indicator of too high/too low/ within range
+* Date and Time of blood sugar value
+* Perhaps an analysis of the various food with the history of the sugar values? -- maybe later version
+*
+* Food Class to store nutrition info and calculation measurements based on portion size
+* Meal class? or List of Meals per day? 
+* Read/Write File to import food data
+* CRUD food data (don't forget beverages)
+* CRUD sugar data
+* Log blood sugars? For future reference?
+*	Get time of reading
+*	Get value of reading
+*	Determine concern (within range, too low, too high)
+*	//associate with meal/exercise? -- maybe later version
+*	//Look for trends based on time of day or food consumed -- maybe later version
+* Log meals for future reference?
+* 	Get Time of meal
+*	***While mealIncomplete***
+*	Get food - if not in system - add
+*	State what portion is for food prior to asking
+*	Get portion size
+*	Finish getting all food - then show calories, protein, carbs, fiber
+* Show daily values
+*	Get all meals for day
+*	Sum calculations for each
+*/
+
+public class Nutrition{
+	public static void main (String [] args){
+		System.out.println("Hello World!");
+	}
+}
